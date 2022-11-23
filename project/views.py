08fcff9ba1f4c0f5a1799b5600413bee255ac60b
Anton Maslennikov <anton.maslennikov@gmail.com>
2022-11-23 20:53:54 +0300

    +

--- project/views.py
+++ project/views.py
@@ -2,16 +2,19 @@ from django.shortcuts import render
 from project.models import Project
 from django.contrib.auth.decorators import login_required
 
+def getmyprojects():
+    projects = projects = Project.objects.all()
+    return projects
+
 @login_required
 def index(request):
-    projects = Project.objects.all()
-    return render(request, 'project/index.html', {'projects': projects})
+    return render(request, 'project/index.html', {'projects': getmyprojects})
 
 @login_required
 def detail(request, pk):
     project = Project.objects.get(pk=pk)
-    return render(request, 'project/detail.html', {'project': project})
+    return render(request, 'project/detail.html', {'projects': getmyprojects, 'project': project,})
 
 @login_required
 def create(request):
-    return render(request, 'project/detail.html')
+    return render(request, 'project/create.html', {'projects': getmyprojects})
