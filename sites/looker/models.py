0495f7a3f0ca43de97ee85e2b580da607e5131d5
clofsj <clofsj@ya.ru>
2023-06-22 03:12:43 -0400

    change models.py

--- sites/looker/models.py
+++ sites/looker/models.py
@@ -1,120 +1,73 @@
 from django.db import models
 
-
 class Clients(models.Model):
-    client_name = models.CharField(max_length=128, blank=True, null=True)
-    date_add = models.DateField(blank=True, null=True)
-
-    def __str__(self):
-        return self.client_name
-
-    class Meta:
-        db_table = 'clients'
-
+    id = models.IntegerField(primary_key=True)
+    client_name = models.CharField(max_length=128, null=True, default=None)
+    date_add = models.DateField(null=True, default=None)
 
 class LinksAllAnchors(models.Model):
-    anchor_value = models.TextField(blank=True, null=True)
-    date_add = models.DateField(blank=True, null=True)
-
-    def __str__(self):
-        return self.anchor_value
-
-    class Meta:
-        db_table = 'links_all_anchors'
-
+    id = models.IntegerField(primary_key=True)
+    anchor_value = models.TextField()
+    date_add = models.DateField(null=True, default=None)
 
 class LinksAllDomains(models.Model):
-    domain_name = models.CharField(max_length=128, blank=True, null=True)
-    date_add = models.DateField(blank=True, null=True)
-
-    def __str__(self):
-        return self.domain_name
-
-    class Meta:
-        db_table = 'links_all_domains'
-
+    id = models.IntegerField(primary_key=True)
+    domain_name = models.CharField(max_length=128, null=True, default=None)
+    date_add = models.DateField(null=True, default=None)
 
 class LinksAllUrls(models.Model):
-    url = models.TextField(blank=True, null=True)
-    date_add = models.DateField(blank=True, null=True)
-    domain = models.ForeignKey(LinksAllDomains, on_delete=models.CASCADE, db_column='id_domain', blank=True, null=True)
-
-    def __str__(self):
-        return self.url
-
-    class Meta:
-        db_table = 'links_all_urls'
-
+    id = models.IntegerField(primary_key=True)
+    url = models.TextField()
+    date_add = models.DateField(null=True, default=None)
+    id_domain = models.IntegerField(null=True, default=None)
+    domain = models.ForeignKey(LinksAllDomains, on_delete=models.CASCADE)
 
 class LinksCheckDonorAcceptor(models.Model):
-    url_from_donor = models.ForeignKey(LinksAllUrls, on_delete=models.CASCADE, db_column='id_url_from_donor')
-    url_to_acceptor = models.ForeignKey(
-        LinksAllUrls,
-        on_delete=models.CASCADE,
-        db_column='id_url_to_acceptor',
-        related_name='linkscheckdonoracceptor_id_url_to_acceptor_set'
-    )
-    anchor = models.ForeignKey(LinksAllAnchors, on_delete=models.CASCADE, db_column='id_anchor', blank=True, null=True)
-    date_check = models.DateField(blank=True, null=True)
-
-    class Meta:
-        db_table = 'links_check_donor_acceptor'
-        unique_together = (('url_from_donor', 'url_to_acceptor'),)
-
+    id = models.IntegerField(primary_key=True)
+    id_url_from_donor = models.IntegerField()
+    id_url_to_acceptor = models.IntegerField()
+    id_anchor = models.IntegerField(null=True, default=None)
+    date_check = models.DateField(null=True, default=None)
+    donor_url = models.ForeignKey(LinksAllUrls, related_name='donor_links', on_delete=models.CASCADE)
+    acceptor_url = models.ForeignKey(LinksAllUrls, related_name='acceptor_links', on_delete=models.CASCADE)
+    anchor = models.ForeignKey(LinksAllAnchors, null=True, default=None, on_delete=models.CASCADE)
 
 class MoneySites(models.Model):
-    site_url = models.CharField(max_length=100, blank=True, null=True)
+    id_row = models.IntegerField(primary_key=True)
+    site_url = models.CharField(max_length=100, null=True, default=None)
+    client_id = models.IntegerField()
+    id_domain = models.IntegerField(null=True, default=None)
     client = models.ForeignKey(Clients, on_delete=models.CASCADE)
-    domain = models.ForeignKey(LinksAllDomains, on_delete=models.CASCADE, db_column='id_domain', blank=True, null=True)
-
-    def __str__(self):
-        return self.site_url
-
-    class Meta:
-        db_table = 'money_sites'
-
+    domain = models.ForeignKey(LinksAllDomains, null=True, default=None, on_delete=models.CASCADE)
 
 class PbnArticles(models.Model):
-    id_article = models.IntegerField(primary_key=True)
-    pbn_site = models.ForeignKey('PbnSites', on_delete=models.CASCADE, db_column='id_pbn_site')
-    date_create = models.DateTimeField(blank=True, null=True)
-    date_modified = models.DateTimeField(blank=True, null=True)
-    text_article = models.TextField(blank=True, null=True)
-    article_name = models.TextField(blank=True, null=True)
-    article_url = models.TextField(blank=True, null=True)
-
-    class Meta:
-        db_table = 'pbn_articles'
-
+    id_row = models.IntegerField(primary_key=True)
+    id_article = models.IntegerField()
+    id_pbn_site = models.IntegerField()
+    date_create = models.DateTimeField(null=True, default=None)
+    date_modified = models.DateTimeField(null=True, default=None)
+    text_article = models.TextField()
+    article_name = models.TextField()
+    article_url = models.TextField()
+    pbn_site = models.ForeignKey(PbnSites, on_delete=models.CASCADE)
 
 class PbnSites(models.Model):
-    site_url = models.CharField(max_length=128, blank=True, null=True)
-    date_create = models.DateTimeField(blank=True, null=True)
-    server = models.ForeignKey('Servers', on_delete=models.CASCADE, db_column='id_server', blank=True, null=True)
-
-    def __str__(self):
-        return self.site_url
-
-    class Meta:
-        db_table = 'pbn_sites'
-
+    id = models.IntegerField(primary_key=True)
+    site_url = models.CharField(max_length=128, null=True, default=None)
+    date_create = models.DateTimeField(null=True, default=None)
+    id_server = models.IntegerField(null=True, default=None)
+    server = models.ForeignKey(Servers, on_delete=models.CASCADE)
 
 class RelationPbnSitesLinksAllDomains(models.Model):
-    id_pbn_sites = models.IntegerField(primary_key=True)
-    id_links_all_domains = models.IntegerField
-
-    class Meta:
-        db_table = 'relation_pbn_sites_links_all_domains'
-
+    id_pbn_sites = models.IntegerField()
+    id_links_all_domains = models.IntegerField()
+    pbn_site = models.ForeignKey(PbnSites, on_delete=models.CASCADE)
+    links_all_domain = models.ForeignKey(LinksAllDomains, on_delete=models.CASCADE)
 
 class Servers(models.Model):
-    server_name = models.CharField(max_length=64, blank=True, null=True)
-    ip_server = models.CharField(max_length=15, blank=True, null=True)
-    client = models.ForeignKey(Clients, on_delete=models.CASCADE)
-    date_add = models.DateField(blank=True, null=True)
-
-    def __str__(self):
-        return self.server_name
-
-    class Meta:
-        db_table = 'servers'
+    id = models.IntegerField(primary_key=True)
+    server_name = models.CharField(max_length=64, null=True, default=None)
+    ip_server = models.CharField(max_length=15, null=True, default=None)
+    client_id = models.IntegerField(null=True, default=None)
+    date_add = models.DateField(null=True, default=None)
+    client = models.ForeignKey(Clients, null=True, default=None, on_delete=models.CASCADE)
