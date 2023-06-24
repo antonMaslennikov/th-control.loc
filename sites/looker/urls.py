18bfc30bbcd89fe21e717f5d76dde39f078ff57e
clofsj <clofsj@ya.ru>
2023-06-24 01:04:55 -0400

    change views

--- sites/looker/urls.py
+++ sites/looker/urls.py
@@ -1,11 +1,32 @@
 from django.urls import path
-from .views import get_publications_chart, domain_pbn_and_publications, index, clients_list
+from .views import (
+    LinksAllDomainsAPIView, LinksAllUrlsAPIView, LinksCheckDonorAcceptorAPIView,
+    PbnSitesAPIView, RelationPbnSitesLinksAllDomainsAPIView, MoneySitesAPIView,
+    PbnArticlesAPIView, get_publications_chart, domain_pbn_and_publications
+)
 
 app_name = 'looker'
+
 urlpatterns = [
-    path('', index),
-    path('api/clients', clients_list),
+    path('api/links-all-domains/', LinksAllDomainsAPIView.as_view(), name='links_all_domains_api'),
+    path('api/links-all-domains/<int:domain_id>/', LinksAllDomainsAPIView.as_view(),
+         name='links_all_domains_detail_api'),
+    path('api/links-all-urls/', LinksAllUrlsAPIView.as_view(), name='links_all_urls_api'),
+    path('api/links-all-urls/<int:url_id>/', LinksAllUrlsAPIView.as_view(), name='links_all_urls_detail_api'),
+    path('api/links-check-donor-acceptor/', LinksCheckDonorAcceptorAPIView.as_view(),
+         name='links_check_donor_acceptor_api'),
+    path('api/links-check-donor-acceptor/<int:donor_acceptor_id>/', LinksCheckDonorAcceptorAPIView.as_view(),
+         name='links_check_donor_acceptor_detail_api'),
+    path('api/pbn-sites/', PbnSitesAPIView.as_view(), name='pbn_sites_api'),
+    path('api/pbn-sites/<int:pbn_site_id>/', PbnSitesAPIView.as_view(), name='pbn_sites_detail_api'),
+    path('api/relation-pbn-sites-links-all-domains/', RelationPbnSitesLinksAllDomainsAPIView.as_view(),
+         name='relation_pbn_sites_links_all_domains_api'),
+    path('api/relation-pbn-sites-links-all-domains/<int:relation_id>/',
+         RelationPbnSitesLinksAllDomainsAPIView.as_view(), name='relation_pbn_sites_links_all_domains_detail_api'),
+    path('api/money-sites/', MoneySitesAPIView.as_view(), name='money_sites_api'),
+    path('api/money-sites/<int:money_site_id>/', MoneySitesAPIView.as_view(), name='money_sites_detail_api'),
+    path('api/pbn-articles/', PbnArticlesAPIView.as_view(), name='pbn_articles_api'),
+    path('api/pbn-articles/<int:article_id>/', PbnArticlesAPIView.as_view(), name='pbn_articles_detail_api'),
     path('api/get-publications-chart', get_publications_chart),
     path('api/domain-pbn-and-publications', domain_pbn_and_publications),
-
 ]
--- sites/looker/views.py
+++ sites/looker/views.py
@@ -1,21 +1,119 @@
-from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
 from .service import get_domain_and_pbn_publications, get_publications_by_client
-from .models import Clients, LinksAllAnchors, Servers
+
+from django.http import JsonResponse
+from django.views import View
+from .models import (
+    LinksAllDomains, LinksAllUrls, LinksCheckDonorAcceptor, PbnSites,
+    RelationPbnSitesLinksAllDomains, MoneySites, PbnArticles,
+)
 from .settings import DB
 
 
-def index(request):
-    return render(request, 'looker/index.html')
+class LinksAllDomainsAPIView(View):
+    def get(self, request, domain_id=None):
+        if domain_id:
+            domain = LinksAllDomains.objects.using(DB).filter(id=domain_id).first()
+            if domain:
+                data = domain.to_json()
+            else:
+                data = {}
+        else:
+            domains = LinksAllDomains.objects.using(DB).all()
+            data = [domain.to_json() for domain in domains]
+
+        return JsonResponse(data, safe=False)
 
 
-@csrf_exempt
-def clients_list(request):
-    if request.method == 'GET':
-        clients = Clients.objects.using(DB).all()
-        data = [{'id': client.pk, 'client_name': client.client_name, 'date_add': client.date_add} for client in
-                clients]
+class LinksAllUrlsAPIView(View):
+    def get(self, request, url_id=None):
+        if url_id:
+            url = LinksAllUrls.objects.using(DB).filter(id=url_id).first()
+            if url:
+                data = url.to_json()
+            else:
+                data = {}
+        else:
+            urls = LinksAllUrls.objects.using(DB).all()
+            data = [url.to_json() for url in urls]
+
+        return JsonResponse(data, safe=False)
+
+
+class LinksCheckDonorAcceptorAPIView(View):
+    def get(self, request, donor_acceptor_id=None):
+        if donor_acceptor_id:
+            donor_acceptor = LinksCheckDonorAcceptor.objects.using(DB).filter(id=donor_acceptor_id).first()
+            if donor_acceptor:
+                data = donor_acceptor.to_json()
+            else:
+                data = {}
+        else:
+            donor_acceptors = LinksCheckDonorAcceptor.objects.using(DB).all()
+            data = [da.to_json() for da in donor_acceptors]
+
+        return JsonResponse(data, safe=False)
+
+
+class PbnSitesAPIView(View):
+    def get(self, request, pbn_site_id=None):
+        if pbn_site_id:
+            pbn_site = PbnSites.objects.using(DB).filter(id=pbn_site_id).first()
+            if pbn_site:
+                data = pbn_site.to_json()
+            else:
+                data = {}
+        else:
+            pbn_sites = PbnSites.objects.using(DB).all()
+            data = [pbn_site.to_json() for pbn_site in pbn_sites]
+
+        return JsonResponse(data, safe=False)
+
+
+class RelationPbnSitesLinksAllDomainsAPIView(View):
+    def get(self, request, relation_id=None):
+        if relation_id:
+            relation = RelationPbnSitesLinksAllDomains.objects.using(DB).filter(id=relation_id).first()
+            if relation:
+                data = relation.to_json()
+            else:
+                data = {}
+        else:
+            relations = RelationPbnSitesLinksAllDomains.objects.using(DB).all()
+            data = [relation.to_json() for relation in relations]
+
+        return JsonResponse(data, safe=False)
+
+
+class MoneySitesAPIView(View):
+    def get(self, request, money_site_id=None):
+        if money_site_id:
+            money_site = MoneySites.objects.using(DB).filter(id=money_site_id).first()
+            if money_site:
+                data = money_site.to_json()
+            else:
+                data = {}
+        else:
+            money_sites = MoneySites.objects.using(DB).all()
+            data = [money_site.to_json() for money_site in money_sites]
+
+        return JsonResponse(data, safe=False)
+
+
+class PbnArticlesAPIView(View):
+    def get(self, request, article_id=None):
+        if article_id:
+            article = PbnArticles.objects.using(DB).filter(id=article_id).first()
+            if article:
+                data = article.to_json()
+            else:
+                data = {}
+        else:
+            articles = PbnArticles.objects.using(DB).all()
+            data = [article.to_json() for article in articles]
+
         return JsonResponse(data, safe=False)
 
 
@@ -34,12 +132,4 @@ def domain_pbn_and_publications(request, client_id):
 def get_publications_chart(request, client_id=None):
     if request.method == 'GET':
         data = get_publications_by_client(client_id)
-        return JsonResponse(data, safe=False)
-
-
-@csrf_exempt
-def links_anchor(request, id):
-    if request.method == 'GET':
-        anchors = LinksAllAnchors.objects.using(DB).all()
-        data = [{'anchor_value': anchor.anchor_value, 'date_add': anchor.date_add} for anchor in anchors]
         return JsonResponse(data, safe=False)
