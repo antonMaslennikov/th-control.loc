0189a53c305c236e1b5463a7380512c7fc32097d
Anton Maslennikov <anton.maslennikov@gmail.com>
2023-07-04 10:38:49 +0300

    +

--- project/models.py
+++ project/models.py
@@ -52,7 +52,7 @@ class Service(models.Model):
 
     name = models.CharField(max_length=255)
     service_class = models.IntegerField(choices=STATUS, null=True)
-    site = models.CharField(max_length=1000, blank=True, null=True)
+    # site = models.CharField(max_length=1000, blank=True, null=True)
     description = models.CharField(max_length=1000, blank=True, null=True)
     settings = models.ManyToManyField(Setting, blank=True)
 
