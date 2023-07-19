9699f444882d4c3b056b935e58cfeedd0a6461d7
clofsj <clofsj@ya.ru>
2023-07-19 19:20:34 +0500

    chhange service

--- services/looker_studio/lib/sync_plan_pbn.py
+++ services/looker_studio/lib/sync_plan_pbn.py
@@ -1,32 +1,51 @@
 from googleapiclient.discovery import build
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
+import sys
 
-# Load the access token from file
-credentials = Credentials.from_authorized_user_file('token.json')
+sys.path.append('../../../sites')
+from sites.looker.models import PbnPlans
 
-# Check if the access token has expired and refresh if necessary
-if credentials.expired and credentials.refresh_token:
-    credentials.refresh(Request())
 
-# Build the service
-service = build('sheets', 'v4', credentials=credentials)
+def fetch_spreadsheet_data():
+    # Load the access token from file
+    credentials = Credentials.from_authorized_user_file('token.json')
 
-# Read the spreadsheet data
-spreadsheet_id = '1lr605ApFV2Mdoevc4HZxi1qiPmuuLfIV-ccb2wfg4H4'
-range_ = '!A1:Z'
-response = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range=range_).execute()
-values = response.get('values', [])
+    # Check if the access token has expired and refresh if necessary
+    if credentials.expired and credentials.refresh_token:
+        credentials.refresh(Request())
 
-# Extract the column names
-column_names = values[0]
+    # Build the service
+    service = build('sheets', 'v4', credentials=credentials)
 
-# Convert the remaining rows to a list of dictionaries
-data = []
-for row in values[1:]:
-    row_dict = {column_names[i]: value for i, value in enumerate(row)}
-    data.append(row_dict)
+    # Read the spreadsheet data
+    spreadsheet_id = '1lr605ApFV2Mdoevc4HZxi1qiPmuuLfIV-ccb2wfg4H4'
+    range_ = '!A1:E'  # Adjust the range to match the columns in the spreadsheet
+    response = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range=range_).execute()
+    values = response.get('values', [])
 
-# Print the data
-for item in data:
-    print(item)
+    # Extract the column names
+    column_names = values[0]
+
+    # Convert the remaining rows to a list of dictionaries
+    data = []
+    for row in values[1:]:
+        row_dict = {column_names[i]: value for i, value in enumerate(row)}
+        data.append(row_dict)
+
+    return data
+
+
+def clear_and_save_data():
+    PbnPlans.objects.using("looker_db").all().delete()
+    spreadsheet_data = fetch_spreadsheet_data()
+    print(spreadsheet_data)
+    for row_dict in spreadsheet_data:
+        pbn_plan = PbnPlans(
+            client=row_dict.get('client'),
+            money_site=row_dict.get('money_site'),
+            pbn_sites=row_dict.get('pbn_sites'),
+            links=row_dict.get('links'),
+            deadline=row_dict.get('deadline')
+        )
+        pbn_plan.save()
